53865f8624555178b21961309f7d2271f934cba7
Roger Luo <Rawger@Rawger.local>
2018-03-28 04:26:43 -0700

    needs fog and raycast

--- js/sekai.js
+++ js/sekai.js
@@ -3,50 +3,33 @@ document.body.appendChild( container );
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x181817);
-scene.fog = new THREE.Fog(scene.background, 500, 3000)
+// scene.fog = new THREE.FogExp2(scene.background, 0.025, 3000);
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 3000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 1000);
 const clock = new THREE.Clock();
 const controls = new THREE.FlyControls(camera);
 controls.domElement = container;
-controls.movementSpeed = 250;
-// controls.domElement = container;
-// controls.rollSpeed = Math.PI / 6;
-// controls.autoForward = false;
-// controls.dragToLook = false;
+controls.movementSpeed = 30;
+controls.rollSpeed = Math.PI / 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+window.addEventListener('mousemove', (event) => {
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+});
+
 const spriteMap = new THREE.TextureLoader().load('./assets/img/pepper.jpg');
 const spriteMaterial = new THREE.SpriteMaterial({map: spriteMap});
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 3000; i++) {
   const testSprite = new THREE.Sprite( spriteMaterial );
   testSprite.position.set(Math.random()*100-50, Math.random()*100-50, Math.random()*100-50);
   scene.add(testSprite);
 }
-
-// let timer;
-// renderer.domElement.addEventListener('mousemove', event => {
-//   const xoffset = event.clientX - window.innerWidth/2
-//   const yoffset = event.clientY - window.innerHeight/2
-//   camera.rotation.x -= yoffset / 10000;
-//   camera.rotation.y -= xoffset / 10000;
-//
-//   clearTimeout(timer);
-//   timer = setTimeout(() => {
-//     const rot_start = { x:camera.rotation.x, y:camera.rotation.y};
-//     const rot_end = { x:camera.rotation.x - yoffset/1000, y:camera.rotation.y - xoffset/1000};
-//     const rot_tween = new TWEEN.Tween(rot_start).to(rot_end, 500)
-//       .easing(TWEEN.Easing.Quadratic.Out);
-//     rot_tween.onUpdate(function(){
-//       camera.rotation.x = rot_start.x;
-//       camera.rotation.y = rot_start.y;
-//     });
-//     rot_tween.start();
-//   }, 30);
-// })
 
 window.addEventListener('resize', event => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -62,8 +45,14 @@ function animate() {
 function render() {
   const delta = clock.getDelta();
   controls.update(delta);
+  raycaster.setFromCamera( mouse, camera );
+  const intersects = raycaster.intersectObjects( scene.children );
+  console.log(mouse);
+  for (let i = 0; i < intersects.length; i++ ) {
+		intersects[ i ].object.material.color.set( 0x9297C4 );
+	}
   renderer.render(scene, camera);
-  // TWEEN.update();
+
 }
 
 animate();
