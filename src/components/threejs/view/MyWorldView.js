b87c46fb4b5d05b30de667855f0c3f2766c10c07
Roger Luo <rawger@berkeley.edu>
2018-05-23 20:04:26 -0700

    Pre-React things now visible

---
+++ src/components/threejs/view/MyWorldView.js
@@ -0,0 +1,10 @@
+import * as THREE from three;
+
+import View from './View';
+
+export default class MyWorldView extends View {
+  constructor(renderer, scene, camera, cameraControlsClass) {
+    super(renderer, scene, camera, cameraControlsClass);
+    
+  }
+}
